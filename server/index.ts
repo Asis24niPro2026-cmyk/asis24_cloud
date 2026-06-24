6a23dcdf27a4deb8f0697dfa998df794d2c41741
Asis24niPro2026-cmyk <eveliolab2025@gmail.com>
2026-06-24 15:51:27 -0600

    Add health check endpoint to server

--- server/index.ts
+++ server/index.ts
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ status: "ok" });
+});
+
 app.use(
   "/trpc",
   trpcExpress.createExpressMiddleware({
