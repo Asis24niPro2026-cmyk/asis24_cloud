9c8216ec2d72fad367243b6995943feb57be94ae
Asis24niPro2026-cmyk <eveliolab2025@gmail.com>
2026-07-10 14:00:27 -0600

    Refactor App component to use React Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,20 @@
-import Hello from "./components/Hello";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BusinessProfile from "./pages/BusinessProfile";
+import RegisterBusiness from "./pages/RegisterBusiness";
+import AdminPanel from "./pages/AdminPanel";
 
 function App() {
   return (
-    <div>
-      <h1>Mi App de Pedidos</h1>
-      <Hello />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/negocio/:id" element={<BusinessProfile />} />
+        <Route path="/registrar" element={<RegisterBusiness />} />
+        <Route path="/admin/:id" element={<AdminPanel />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-
-
