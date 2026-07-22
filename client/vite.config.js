50bf3b7ce5115534c9cce33979b352c2876a07d1
Asis24niPro2026-cmyk <eveliolab2025@gmail.com>
2026-07-22 14:38:00 -0600

    Remove server port configuration
    Removed server port configuration from Vite config.

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // opcional, si quieres fijar el puerto en local
-  },
   preview: {
     allowedHosts: [
-      "asis24-cloud-1.onrender.com" // tu backend en Render
+      "asis24-cloud-1.onrender.com"
     ]
   }
 });
